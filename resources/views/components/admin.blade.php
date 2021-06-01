c45e3a5ce07e23b126c13906e7cae19aede63772
vlada <vladapantic@hotmail.com>
2021-06-01 10:15:45 +0200

    Install Laravel and Laravel UI

---
+++ resources/views/components/admin.blade.php
@@ -0,0 +1,152 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- CSRF Token -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
+    <title>{{ config('app.name', 'Laravel') }}</title>
+
+    <!-- CoreUI CSS -->
+    <link rel="stylesheet" href="https://unpkg.com/@coreui/coreui/dist/css/coreui.min.css" crossorigin="anonymous">
+    <link rel="stylesheet"
+        href="https://cdnjs.cloudflare.com/ajax/libs/jquery.perfect-scrollbar/1.5.0/css/perfect-scrollbar.min.css"
+        integrity="sha512-n+g8P11K/4RFlXnx2/RW1EZK25iYgolW6Qn7I0F96KxJibwATH3OoVCQPh/hzlc4dWAwplglKX8IVNVMWUUdsw=="
+        crossorigin="anonymous" referrerpolicy="no-referrer" />
+</head>
+
+<body class="c-app">
+    @include('partials.sidebar')
+
+    <div class="c-wrapper c-fixed-components">
+        <header class="c-header c-header-light c-header-fixed c-header-with-subheader">
+            <button class="c-header-toggler c-class-toggler d-lg-none mfe-auto" type="button" data-target="#sidebar" data-class="c-sidebar-show">
+                <svg class="c-icon c-icon-lg">
+                    <use xlink:href="vendors/@coreui/icons/svg/free.svg#cil-menu"></use>
+                </svg>
+            </button>
+            <ul class="c-header-nav ml-auto mr-4">
+                <li class="c-header-nav-item d-md-down-none mx-2">
+                    <a class="c-header-nav-link" href="#">
+                        <svg class="c-icon">
+                            <use xlink:href="{{ asset('vendors/@coreui/icons/svg/free.svg#cil-bell') }}"></use>
+                        </svg>
+                    </a>
+                </li>
+                <li class="c-header-nav-item d-md-down-none mx-2">
+                    <a class="c-header-nav-link" href="#">
+                        <svg class="c-icon">
+                            <use xlink:href="vendors/@coreui/icons/svg/free.svg#cil-list-rich"></use>
+                        </svg>
+                    </a>
+                </li>
+                <li class="c-header-nav-item d-md-down-none mx-2">
+                    <a class="c-header-nav-link" href="#">
+                        <svg class="c-icon">
+                            <use xlink:href="vendors/@coreui/icons/svg/free.svg#cil-envelope-open"></use>
+                        </svg>
+                    </a>
+                </li>
+                <li class="c-header-nav-item dropdown">
+                    <a class="c-header-nav-link" data-toggle="dropdown" href="#"
+                        role="button" aria-haspopup="true" aria-expanded="false">
+                        <div class="c-avatar">
+                            <img class="c-avatar-img" src="assets/img/avatars/6.jpg"
+                                alt="user@email.com">
+                        </div>
+                    </a>
+                    <div class="dropdown-menu dropdown-menu-right pt-0">
+                        <div class="dropdown-header bg-light py-2"><strong>Account</strong></div>
+                        <a
+                            class="dropdown-item" href="#">
+                            <svg class="c-icon mr-2">
+                                <use xlink:href="vendors/@coreui/icons/svg/free.svg#cil-bell"></use>
+                            </svg> Updates<span class="badge badge-info ml-auto">42</span>
+                        </a>
+                        <a class="dropdown-item"
+                            href="#">
+                            <svg class="c-icon mr-2">
+                                <use xlink:href="vendors/@coreui/icons/svg/free.svg#cil-envelope-open"></use>
+                            </svg> Messages<span class="badge badge-success ml-auto">42</span>
+                        </a>
+                        <a
+                            class="dropdown-item" href="#">
+                            <svg class="c-icon mr-2">
+                                <use xlink:href="vendors/@coreui/icons/svg/free.svg#cil-task"></use>
+                            </svg> Tasks<span class="badge badge-danger ml-auto">42</span>
+                        </a>
+                        <a class="dropdown-item"
+                            href="#">
+                            <svg class="c-icon mr-2">
+                                <use xlink:href="vendors/@coreui/icons/svg/free.svg#cil-comment-square"></use>
+                            </svg> Comments<span class="badge badge-warning ml-auto">42</span>
+                        </a>
+                        <div class="dropdown-header bg-light py-2"><strong>Settings</strong></div>
+                        <a
+                            class="dropdown-item" href="#">
+                            <svg class="c-icon mr-2">
+                                <use xlink:href="vendors/@coreui/icons/svg/free.svg#cil-user"></use>
+                            </svg> Profile
+                        </a>
+                        <a class="dropdown-item" href="#">
+                            <svg class="c-icon mr-2">
+                                <use xlink:href="vendors/@coreui/icons/svg/free.svg#cil-settings"></use>
+                            </svg> Settings
+                        </a>
+                        <a class="dropdown-item" href="#">
+                            <svg class="c-icon mr-2">
+                                <use xlink:href="vendors/@coreui/icons/svg/free.svg#cil-credit-card"></use>
+                            </svg> Payments<span class="badge badge-secondary ml-auto">42</span>
+                        </a>
+                        <a
+                            class="dropdown-item" href="#">
+                            <svg class="c-icon mr-2">
+                                <use xlink:href="vendors/@coreui/icons/svg/free.svg#cil-file"></use>
+                            </svg> Projects<span class="badge badge-primary ml-auto">42</span>
+                        </a>
+                        <div class="dropdown-divider"></div>
+                        <a class="dropdown-item" href="#">
+                            <svg class="c-icon mr-2">
+                                <use xlink:href="vendors/@coreui/icons/svg/free.svg#cil-lock-locked"></use>
+                            </svg> Lock Account
+                        </a>
+                        <a class="dropdown-item" href="{{ route('logout') }}"
+                            onclick="event.preventDefault();
+                                document.getElementById('logout-form').submit();">
+                            <svg class="c-icon mr-2">
+                                <use xlink:href="vendors/@coreui/icons/svg/free.svg#cil-account-logout"></use>
+                            </svg> {{ __('Logout') }}
+                        </a>
+                    </div>
+                </li>
+            </ul>
+        </header>
+        <div class="c-body">
+            <main class="c-main">
+                <div class="container-fluid">
+                    <div class="fade-in">
+                        {{ $slot }}
+                    </div>
+                </div>
+            </main>
+            <footer class="c-footer">
+                <div><a href="https://coreui.io">CoreUI</a> © 2020 creativeLabs.</div>
+                <div class="ml-auto">Powered by&nbsp;<a href="https://coreui.io/">CoreUI</a></div>
+            </footer>
+        </div>
+    </div>
+
+    <!-- Optional JavaScript -->
+    <!-- Popper.js first, then CoreUI JS -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery.perfect-scrollbar/1.5.0/perfect-scrollbar.min.js"
+        integrity="sha512-yUNtg0k40IvRQNR20bJ4oH6QeQ/mgs9Lsa6V+3qxTj58u2r+JiAYOhOW0o+ijuMmqCtCEg7LZRA+T4t84/ayVA=="
+        crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+    <script src="https://unpkg.com/@popperjs/core@2"></script>
+    <script src="https://unpkg.com/@coreui/coreui/dist/js/coreui.min.js"></script>
+</body>
+
+</html>
---
+++ resources/views/layouts/auth.blade.php
@@ -0,0 +1,39 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- CSRF Token -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
+    <title>{{ config('app.name', 'Laravel') }}</title>
+
+    <!-- CoreUI CSS -->
+    <link rel="stylesheet" href="https://unpkg.com/@coreui/coreui/dist/css/coreui.min.css" crossorigin="anonymous">
+</head>
+
+<body class="c-app flex-row align-items-center">
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-12">
+                <div class="card-group">
+                    <div class="card p-4">
+                        <div class="card-body">
+                            @yield('content')
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <!-- Optional JavaScript -->
+    <!-- Popper.js first, then CoreUI JS -->
+    <script src="https://unpkg.com/@popperjs/core@2"></script>
+    <script src="https://unpkg.com/@coreui/coreui/dist/js/coreui.min.js"></script>
+</body>
+
+</html>
