82856c70e9f535d1eec44aefbc361b731f48c1be
vlada <vladapantic@hotmail.com>
2021-06-01 14:31:20 +0200

    Fix asset loading, fix some css

--- resources/views/partials/sidebar.blade.php
+++ resources/views/partials/sidebar.blade.php
@@ -1,41 +1,26 @@
 <div class="c-sidebar c-sidebar-dark c-sidebar-fixed c-sidebar-lg-show" id="sidebar">
-    <div class="c-sidebar-brand d-lg-down-none">
-        <svg class="c-sidebar-brand-full" width="118" height="46" alt="CoreUI Logo">
-            <use xlink:href="assets/brand/coreui.svg#full"></use>
-        </svg>
-        <svg class="c-sidebar-brand-minimized" width="46" height="46" alt="CoreUI Logo">
-            <use xlink:href="assets/brand/coreui.svg#signet"></use>
-        </svg>
-    </div>
     <ul class="c-sidebar-nav ps">
         <li class="c-sidebar-nav-item">
             <a class="c-sidebar-nav-link" href="{{ route('home') }}">
                 <svg class="c-sidebar-nav-icon">
-                    <use xlink:href="vendors/@coreui/icons/svg/free.svg#cil-speedometer"></use>
+                    <use xlink:href="{{ asset('vendors/@coreui/icons/svg/free.svg#cil-speedometer') }}"></use>
                 </svg> Dashboard
             </a>
         </li>
-        <li class="c-sidebar-nav-title">Components</li>
-        <li class="c-sidebar-nav-item c-sidebar-nav-dropdown"><a
-                class="c-sidebar-nav-link c-sidebar-nav-dropdown-toggle" href="#">
+        <li class="c-sidebar-nav-title">Manage projects</li>
+        <li class="c-sidebar-nav-item">
+            <a class="c-sidebar-nav-link" href="{{ route('projects.index') }}">
                 <svg class="c-sidebar-nav-icon">
-                    <use xlink:href="vendors/@coreui/icons/svg/free.svg#cil-puzzle"></use>
-                </svg> Base</a>
-            <ul class="c-sidebar-nav-dropdown-items">
-                <li class="c-sidebar-nav-item">
-                    <a class="c-sidebar-nav-link" href="base/breadcrumb.html">
-                        <span class="c-sidebar-nav-icon"></span> 
-                        Breadcrumb
-                    </a>
-                </li>
-            </ul>
+                    <use xlink:href="{{ asset('vendors/@coreui/icons/svg/free.svg#cil-puzzle') }}"></use>
+                </svg> {{ __('All Projects') }}
+            </a>
         </li>
         <li class="c-sidebar-nav-item">
             <a class="c-sidebar-nav-link" href="{{ route('logout') }}"
                 onclick="event.preventDefault();
                     document.getElementById('logout-form').submit();">
                 <svg class="c-sidebar-nav-icon">
-                    <use xlink:href="vendors/@coreui/icons/svg/free.svg#cil-account-logout"></use>
+                    <use xlink:href="{{ asset('vendors/@coreui/icons/svg/free.svg#cil-account-logout') }}"></use>
                 </svg>
                 {{ __('Logout') }}
             </a>
--- resources/views/projects/index.blade.php
+++ resources/views/projects/index.blade.php
@@ -1,29 +1,38 @@
 <x-admin>
-    <div class="col-lg-12">
-        <div class="card">
-            <div class="card-header"><i class="fa fa-align-justify"></i> {{ __('Projects list') }}</div>
-            <div class="card-body">
-                <table class="table table-responsive-sm table-striped">
-                    <thead>
-                        <tr>
-                            <th>Owner</th>
-                            <th>Date created</th>
-                            <th>Title</th>
-                            <th>Status</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        @foreach ($projects as $project)
-                            <tr>
-                                <td>{{ $project->user->name }}</td>
-                                <td>{{ $project->created_at }}</td>
-                                <td>{{ $project->title }}</td>
-                                <td><span class="badge badge-success">Active</span></td>
-                            </tr>
-                        @endforeach
-                    </tbody>
-                </table>
-                {{ $projects->links() }}
+    <div class="container-fluid">
+        <div class="fade-in">
+            <div class="row">
+                <div class="col-lg-12">
+                    <div class="card">
+                        <div class="card-body">
+                            <table class="table table-responsive-sm table-striped">
+                                <thead>
+                                    <tr>
+                                        <th>Owner</th>
+                                        <th>Date created</th>
+                                        <th>Title</th>
+                                        <th>Status</th>
+                                        <th>Action</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    @foreach ($projects as $project)
+                                        <tr>
+                                            <td>{{ $project->user->name }}</td>
+                                            <td>{{ $project->created_at }}</td>
+                                            <td>{{ $project->title }}</td>
+                                            <td><span class="badge badge-success">Active</span></td>
+                                            <td>
+                                                <a href="{{ route('projects.edit', $project) }}" class="btn btn-info btn-sm" type="button">{{ __('Edit') }}</a>
+                                            </td>
+                                        </tr>
+                                    @endforeach
+                                </tbody>
+                            </table>
+                            {{ $projects->links() }}
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
