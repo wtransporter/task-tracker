cca6e8ef64d50e52fe3eec9a7d2dca55449d1be4
VPantic <vladapantic@hotmail.com>
2021-06-02 18:22:11 +0200

    Display user posts on home page

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -23,6 +23,7 @@ class HomeController extends Controller
      */
     public function index()
     {
-        return view('home');
+        $projects = auth()->user()->projects()->get();
+        return view('home', compact('projects'));
     }
 }
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,18 +1,38 @@
 <x-admin>
-    <div class="container">
-        <div class="row justify-content-center">
-            <div class="col-md-12">
-                <div class="card">
-                    <div class="card-header">{{ __('Dashboard') }}</div>
+    <div class="container-fluid">
+        <div class="fade-in">
+            <div class="row">
+                <div class="col-lg-12">
+                    <div class="card">
+                        <div class="card-header">{{ __('Projects') }}</div>
+                        <div class="card-body">
+                            @if (session('status'))
+                                <div class="alert alert-success" role="alert">
+                                    {{ session('status') }}
+                                </div>
+                            @endif
 
-                    <div class="card-body">
-                        @if (session('status'))
-                            <div class="alert alert-success" role="alert">
-                                {{ session('status') }}
+                            <div class="row">
+                            @foreach ($projects as $project)
+                                <div class="col-md-4 col-xl-3">
+                                    <div class="card">
+                                        <div class="card-header h6 p-2 m-0">
+                                            <a href="{{ route('projects.show', $project) }}">
+                                                {{ $project->title }}
+                                            </a>
+                                        </div>
+                                        <div class="card-body p-2">
+                                            {!! \Str::limit($project->content, 50) !!}
+                                        </div>
+                                        <div class="card-footer d-flex align-items-center justify-content-between p-2">
+                                            <span class="badge badge-success mr-1">Task</span>
+                                            <small>{{ \Carbon\Carbon::createFromFormat('Y-m-d H:i:s', $project->created_at)->format('m.d.Y') }}</small>
+                                        </div>
+                                    </div>
+                                </div>
+                            @endforeach
                             </div>
-                        @endif
-
-                        {{ __('You are logged in!') }}
+                        </div>
                     </div>
                 </div>
             </div>
