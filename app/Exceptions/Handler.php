67387d8c3bd01c8eecc8756074232ca1fa6049d1
VPantic <vladapantic@hotmail.com>
2021-06-11 19:22:34 +0200

    Fix unauthorized access redirect with message

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -2,8 +2,10 @@
 
 namespace App\Exceptions;
 
-use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
 use Throwable;
+use Illuminate\Auth\Access\AuthorizationException;
+use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
+use Symfony\Component\HttpKernel\Exception\AccessDeniedHttpException;
 
 class Handler extends ExceptionHandler
 {
@@ -36,6 +38,18 @@ class Handler extends ExceptionHandler
     {
         $this->reportable(function (Throwable $e) {
             //
+        });
+
+        $this->renderable(function (AccessDeniedHttpException $e, $request) {
+
+            $previous = $e->getPrevious();
+
+            if ($previous instanceof AuthorizationException) {
+                return redirect()->route('home')
+                    ->withErrors(['error' => '403 | You are not allowed to see this page.']);
+            }
+
+            return null; 
         });
     }
 }
